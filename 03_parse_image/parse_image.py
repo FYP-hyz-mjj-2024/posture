143f4ccb11297cc07a87c8fc9bc7beb004f9e69e
Yanzhen Huang <huangyanzhen0108@163.com>
2024-08-24 10:20:18 +0800

    :bug: Use cv2.imread instead of PIL's Image class for better integration,

--- 03_parse_image/parse_image.py
+++ 03_parse_image/parse_image.py
@@ -5,37 +5,36 @@ from pathlib import Path
 import numpy as np
 
 
-def get_static_image(path):
-    img = Image.open(path)
-    return img
-
-
-def crop_pedestrians(img_file):
+def crop_pedestrians(img_matrix):
     """
     Input an image with multiple-pedestrians, use YOLOv5s to extract sub-images
     of individual pedestrians.
-    :param img_file: An ImageFile object.
+    :param img_matrix: An ImageFile object.
     :return: List of sub images.
     """
 
     # TODO: Local Model
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
+    # Image: BGR -> RGB
+    img_rgb = cv2.cvtColor(img_matrix, cv2.COLOR_BGR2RGB)
+
     # Extracts all the 4-d tuples corresponding to class 'person'
-    results_df = model(img_file).pandas().xyxy[0]
+    results_df = model(img_matrix).pandas().xyxy[0]
     pedestrians = results_df[results_df['name'] == 'person']
 
     # Store cropped images
     cropped_images = []
 
     for idx, row in pedestrians.iterrows():
-        cropped_img = img.crop(tuple(int(row[name]) for name in ['xmin', 'ymin', 'xmax', 'ymax']))
-        cropped_images.append(np.array(cropped_img))
+        xmin, ymin, xmax, ymax = tuple(int(row[name]) for name in ['xmin', 'ymin', 'xmax', 'ymax'])
+        cropped_img = img_rgb[ymin:ymax, xmin:xmax]
+        cropped_images.append(cropped_img)
 
     return cropped_images
 
 
-img = get_static_image("./data/_test/test_img.png")
+img = cv2.imread("./data/_test/test_img.png")
 cropped_pedestrians = crop_pedestrians(img)
 
 for idx, cropped_img in enumerate(cropped_pedestrians):
