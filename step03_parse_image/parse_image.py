d8ecbb3a7507fa8853a58d8e3981be1dd9f12ab1
Yanzhen Huang <huangyanzhen0108@163.com>
2024-08-25 19:48:50 +0800

    :bee: Try to implement the combined program. Need to optimize

--- step03_parse_image/parse_image.py
+++ step03_parse_image/parse_image.py
@@ -2,16 +2,13 @@ import cv2
 import torch
 
 
-def crop_pedestrians(img_matrix):
+def crop_pedestrians(img_matrix, model):
     """
     Input an image with multiple-pedestrians, use YOLOv5s to extract sub-images
     of individual pedestrians.
     :param img_matrix: An ImageFile object.
     :return: List of sub images.
     """
-
-    # TODO: Local Model
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
     # Image: BGR -> RGB
     img_rgb = cv2.cvtColor(img_matrix, cv2.COLOR_BGR2RGB)
@@ -22,18 +19,21 @@ def crop_pedestrians(img_matrix):
 
     # Store cropped images
     cropped_images = []
+    xyxy_sets = []
 
     for idx, row in pedestrians.iterrows():
         xmin, ymin, xmax, ymax = tuple(int(row[name]) for name in ['xmin', 'ymin', 'xmax', 'ymax'])
         cropped_img = img_rgb[ymin:ymax, xmin:xmax]
         cropped_images.append(cropped_img)
+        xyxy_sets.append([xmin, ymin, xmax, ymax])
 
-    return cropped_images
+    return cropped_images, xyxy_sets
 
 
 if __name__ == "__main__":
+    YOLOv5s_model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).to('cuda')
     img = cv2.imread("./data/_test/test_img.png")
-    cropped_pedestrians = crop_pedestrians(img)
+    cropped_pedestrians, _ = crop_pedestrians(img)
 
     for idx, cropped_img in enumerate(cropped_pedestrians):
         cv2.imwrite(f"./data/_test/cropped/{idx}.png", cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR))
